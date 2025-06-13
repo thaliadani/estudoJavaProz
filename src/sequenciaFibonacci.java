2746b74d07f9a79e6017a62678fab5d5b37b7de3
Thalia Danielle <thaliadani2@gmail.com>
2025-06-13 15:36:10 -0300

    Add files via upload

--- src/sequenciaFibonacci.java
+++ src/sequenciaFibonacci.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class sequenciaFibonacci {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Digite um número:");
+        
+        int numero = scanner.nextInt();
+        int primeiro = 1;
+        int segundo = 1;
+        int contador= 2;
+
+        System.out.println("Sequência de Fibonacci com " +  numero + " números:");
+        System.out.print(primeiro + ", " + segundo);
+
+        while (contador < numero) {
+            int proximo = primeiro + segundo;
+            System.out.print(", " + proximo);
+
+            primeiro = segundo;
+            segundo = proximo;
+            contador++;
+
+        }
     }
+
 }
