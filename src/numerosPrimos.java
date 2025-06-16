b5f12c31edc67ad4d10a29beb3939ce6a6dacf70
Thalia Danielle <thaliadani2@gmail.com>
2025-06-16 09:48:01 -0300

    Criar numerosPrimos.java

---
+++ src/numerosPrimos.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class numerosPrimos {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um numero:");
+        int numero = scanner.nextInt();
+        int div = 0;
+        int contador = 1;
+
+        while ( contador <= numero) {
+            int resultado= numero%contador;
+            if(resultado == 0){
+                div++;
+            }
+            contador++;
+        }
+
+       if(div == 2){
+           System.out.println("É primo");
+       }else{
+          System.out.println("Não é primo");
+       }
+    }
+}
