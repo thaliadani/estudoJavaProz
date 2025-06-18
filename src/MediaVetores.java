05f952f6c97467ebcc11777a9978af943a8b3eff
Thalia Danielle <thaliadani2@gmail.com>
2025-06-18 09:28:02 -0300

    Atualizar o MediaVetores.java

--- src/MediaVetores.java
+++ src/MediaVetores.java
@@ -7,22 +7,15 @@ public class MediaVetor {
         int[] vetor1 = new int[5];
         double soma = 0;
 
-        /* vetor1[0] = 1;
-        vetor1[1] = 5;
-        vetor1[2] = 4;
-        vetor1[3] = 1;
-        vetor1[4] = 1;*/
         // Recebendo os valores que o usuário digitar e armazenar dentro do vetor
         // armazenar na posição "i", que começa de zero e vai até a última posicao do vetor
         for (int i = 0; i < 5; i++) {
-            System.out.println("Digite um valor para o primeiro vetor: ");
+            System.out.println("Digite um número: ");
             int num = sc.nextInt();
             vetor1[i] = num;
             soma += vetor1[i];
         }
-
-        System.out.println("Soma: "+ soma);
-        double media = soma / 5;
-        System.out.println("Media: "+ media);
+        double media = soma / veto1.length;
+        System.out.println("A Média dos números é : "+ media);
     }
 }
